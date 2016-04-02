e4b139f6ed1261d9b4f3cb3781c552bfee2426f6
Hana G <individe.duality@gmail.com>
2016-04-01 23:07:10 -0600

    Update MapsActivity (parent, marker drawing)

--- app/src/main/java/com/engo551/plars/app/MapsActivity.java
+++ app/src/main/java/com/engo551/plars/app/MapsActivity.java
@@ -1,7 +1,6 @@
 package com.engo551.plars.app;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -9,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -16,6 +16,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap map;
     private RestaurantDatabaseHelper rDb;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,43 +26,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        intent = getIntent();
+        if (!intent.hasExtra("markerStyle"))
+            throw new AssertionError();
+        if (!intent.hasExtra("markers"))
+            throw new AssertionError();
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-
         final LatLng CALGARY = new LatLng(51.0486, -114.0708);
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(CALGARY, 10));
 
-        rDb = new RestaurantDatabaseHelper(this);
-        SQLiteDatabase db = rDb.getReadableDatabase();
+        String[] markers = intent.getStringArrayExtra("markers");
+        String markerStyle = intent.getStringExtra("markerStyle");
 
-        String[] columns = {"ID", "NAME", "Latitude", "Longitude"};
-        Cursor cursor = db.query("restaurants", columns, null, null, null, null, null);
+        drawMarkers(markers);
 
-        try {
-            while (cursor.moveToNext()) {
-                LatLng latLng = new LatLng(cursor.getDouble(2), cursor.getDouble(3));
-                map.addMarker(new MarkerOptions().position(latLng).title(cursor.getString(1)));
+        //TODO: Center on current user location
+        //TODO: Draw marker according to type
+        //TODO: Add info window click event***add to class? popup thing
+    }
 
-                //TODO: Center on current user location
-                //TODO: Draw marker according to type
-                //TODO: Add info window click event
-            }
+    private boolean drawMarkers(String[] markers){
+        for (int i = 0; i < markers.length; i++) {
+            String[] marker = markers[i].split(",");
+            drawMarker(marker[RestaurantDatabaseHelper.NAME_COL],
+                    marker[RestaurantDatabaseHelper.TYPE_COL],
+                    Double.parseDouble(marker[RestaurantDatabaseHelper.LATITUDE_COL]),
+                    Double.parseDouble(marker[RestaurantDatabaseHelper.LONGITUDE_COL]));
         }
-        finally {
-            cursor.close();
-        }
+        return true;
+    }
+
+    private boolean drawMarker(String title, String snippet, double latitude, double longitude)
+    {
+        LatLng latLng = new LatLng(latitude, longitude);
+        map.addMarker(new MarkerOptions().position(latLng)
+                .title(title)
+                .snippet(snippet));
+        return true;
     }
 }
